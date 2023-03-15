a5b39ff928bcb89211677b4898feace3225906b1
Rohit Patil <74750524+rohit1097@users.noreply.github.com>
2023-03-15 16:41:11 +0530

    Update lamda.tf

--- lamda.tf
+++ lamda.tf
@@ -1,9 +1,16 @@
 
 resource "aws_lambda_function" "terraform_lambda_function" {
-filename                       = "code.py"
+filename                       = "./python/hello-python.py"
 function_name                  = "Test_Lambda_Function"
 role                           = aws_iam_role.lambda.arn
 handler                        = "index.lambda_handler"
 runtime                        = "python3.8"
 depends_on                     = [aws_iam_role_policy_attachment.attach_iam_policy_to_iam_role]
+}
+
+
+data "archive_file" "zip_the_python_code" {
+type        = "zip"
+source_dir  = "./python/code.py"
+output_path = "./python/hello-python.zip"
 }
