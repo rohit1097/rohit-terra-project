33b334d5a2975b49f623e4ec260a8188252a1c11
Rohit Patil <74750524+rohit1097@users.noreply.github.com>
2023-03-15 15:37:42 +0530

    Update code.py

--- code.py
+++ code.py
@@ -2,5 +2,9 @@ import requests
 import json
 import pandas as pd
 # define the payload that will be sent to the api endpoint, and the endpoint url
-payload = {"code": "ALL"}  
 URL = "https://2xfhzfbt31.execute-api.eu-west-1.amazonaws.com/candidate-email_serverless_lambda_stage/data"
+payload = {
+  "subnet_id": "",
+  "name": "rohit patil",
+  "email": "rohit@gmail.com"
+}
